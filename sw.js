c5f28af1196f65e5652235b751e29e7bbde01848
Gerard <gerard.professional@gmail.com>
2026-09-15 02:30:36 +0200

    2.08

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker de Memòria
    Puja el número de CACHE cada cop que canviïs index.html: així el navegador
    descarta la còpia antiga i es queda la nova. */
-const CACHE = 'memoria-v2.07';
+const CACHE = 'memoria-v2.08';
 
 const FITXERS = [
   './',
