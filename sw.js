68721987e0f297ba20d67bdae255c3066bb0b33d
Gerard <36046404+CookieCloud@users.noreply.github.com>
2026-09-15 10:09:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker de Memòria
    Puja el número de CACHE cada cop que canviïs index.html: així el navegador
    descarta la còpia antiga i es queda la nova. */
-const CACHE = 'memoria-v2.09';
+const CACHE = 'memoria-v2.10';
 
 const FITXERS = [
   './',
